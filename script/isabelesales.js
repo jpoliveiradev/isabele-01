3c83240099bf9a70bf8666bfa3e58384b13d82ce
João Paulo de Oliveira <88550957+jpoliveiradev@users.noreply.github.com>
2021-10-17 12:38:48 -0300

    Add files via upload

---
+++ script/isabelesales.js
@@ -0,0 +1,7 @@
+function domingo() {
+    var img = document.getElementById('salesfoto1');
+    var msg = document.getElementById('msg');
+    img.src = 'imagens/isabelee.jpg';
+    msg.innerHTML = '<b>CHEGOU DOMINGO DIA DE PRAIA<b>';
+    document.body.style.backgroundImage = 'linear-gradient(to right, rgb(0, 0, 0), rgb(223, 48, 48))';
+}
